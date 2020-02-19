216b415d7094fde4cd2352fa261c87d6a29b3dc1
Daniel Andrew <dwandrew@hotmail.com>
2020-02-19 09:05:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      alans_new_info = "GUI"
      update_alan = programmer_hash[:alan_kay][:known_for];
      update_alan = alans_new_info
-     return programmer_hash[:alan_kay][:known_for]
+     return update_alan
      
 end
 
